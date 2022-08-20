612097aefc4ff28d2715bd486675d1309479657a
RickLiu <rick.liu0121@gmail.com>
2022-08-20 15:57:46 -0400

    Added create post page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import LoginPage from "./views/LoginPage";
+import CreatePost from "./views/CreatePost";
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" exact></Route>
+          <Route path="/" exact element={<LoginPage />}></Route>
+          <Route path="/create" exact element={<CreatePost />}></Route>
         </Routes>
       </div>
     </Router>
