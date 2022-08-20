011129d47d22df619c46faeadcb4d5f8c3907c86
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-08-20 18:04:53 -0400

    Complete login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
-import LoginPage from "./views/LoginPage";
+import CustomerLoginPage from "./views/CustomerLoginPage";
+import ContractorLoginPage from "./views/ContractorLoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<LandingPage />} />
-        <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/customer-login" exact element={<CustomerLoginPage />} />
+        <Route
+          path="/contractor-login"
+          exact
+          element={<ContractorLoginPage />}
+        />
       </Routes>
     </Router>
   );
