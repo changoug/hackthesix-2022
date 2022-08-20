b2b3db3408fa337f9713f8828e976471aad45e29
Erick Yan <erick.yanzhang@gmail.com>
2022-08-20 11:29:38 -0400

    Created infowindow pop-up, auth context

--- frontend/src/components/Map/MapInterface.js
+++ frontend/src/components/Map/MapInterface.js
@@ -1,83 +1,47 @@
-import React, { Component, useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '100vw',
-  height: '100vh'
-};
-
-const center = {
-lat: -3.745,
-lng: -38.523
-};
-
+import React, { Component, useState, useRef } from 'react';
+import { GoogleMap, InfoWindow, LoadScript, Marker, useJsApiLoader, DistanceMatrixService } from '@react-google-maps/api';
 
 const MapInterface = () => {
-    const [ currentLocations, setLocations ] = useState([]);
-
-    useEffect( () => {
-        setLocations()
-    }, [])
-
-    const loc = [
-      {
-        name: "Location 1",
-        location: { 
-          lat: 41.3954,
-          lng: 2.162 
-        },
-      },
-      {
-        name: "Location 2",
-        location: { 
-          lat: 41.3917,
-          lng: 2.1649
-        },
-      },
-      {
-        name: "Location 3",
-        location: { 
-          lat: 41.3773,
-          lng: 2.1585
-        },
-      },
-      {
-        name: "Location 4",
-        location: { 
-          lat: 41.3797,
-          lng: 2.1682
-        },
-      },
-      {
-        name: "Location 5",
-        location: { 
-          lat: 41.4055,
-          lng: 2.1915
-        },
-      }
-    ]
+    const [helpLocation, setHelpLocation] = useState({})
 
     const { isLoaded } = useJsApiLoader({
       id: 'google-map-script',
       googleMapsApiKey: "AIzaSyCi4Z6r3IAxS0ywrRniNwvzUFreM7poFyk"
     });
-
-    // const [map, setMap] = React.useState(null)
-
-    // const onLoad = React.useCallback(function callback(map) {
-    //   const bounds = new window.google.maps.LatLngBounds(center);
-    //   map.fitBounds(bounds);
-    //   setMap(map)
-    // }, [])
-  
-    // const onUnmount = React.useCallback(function callback(map) {
-    //   setMap(null)
-    // }, [])
   
     const defaultMapOptions = {
-        streetViewControl: true,
-        fullscreenControl: false
-    }
+      streetViewControl: true,
+      fullscreenControl: false,
+    };
+
+    const containerStyle = {
+      width: '100vw',
+      height: '100vh'
+    };
+    
+    const center = {
+    lat: -3.745,
+    lng: -38.523
+    };
+
+    const helpRequests = [
+      {
+        title: "Clogged Toilet",
+        description: "Help me unclog toilet!",
+        location: {
+          lat: -3.745,
+          lng: -38.523
+        },
+        contact: {
+          phone: "647-647-6477",
+          email: "myemail@gmail.com"
+        }
+      }
+    ]
+
+    console.log(google);
+
+    console.log(calculateRoute());
 
     return ( isLoaded ? (
         <GoogleMap
@@ -85,14 +49,30 @@ const MapInterface = () => {
             mapContainerStyle={containerStyle}
             zoom={15}
             options={defaultMapOptions}
+            onClick={() => setHelpLocation({})}
         >
             {
-              loc.map(item => {
-              console.log(item.name)
-              return (
-              <Marker key={item.name} position={item.location}/>
-              )
+              helpRequests.map(item => {
+                return (
+                  <Marker 
+                  position={item.location}
+                  onClick={() => {
+                    setHelpLocation(item);
+                    calculateRoute()
+                  }}
+                  />
+                )
               })
+            }
+            {
+              helpLocation.location && (
+                <InfoWindow
+                position={helpLocation.location}
+                clickable={true}
+                onCloseClick={() => setHelpLocation({})}>
+                  <p>{helpLocation.title}</p>
+                </InfoWindow>
+              )
             }
         </GoogleMap>
     ) : <></>
