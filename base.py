7053c7507fbb6e1066689a5f1de6e657da73706b
Daniel Fang <daniel@Daniels-MacBook-Air.local>
2022-08-20 13:28:20 -0400

    add database schema

--- base.py
+++ base.py
@@ -1,21 +1,23 @@
-from flask import Flask
-import requests
+import os
+import psycopg2
+from flask import Flask, render_template
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/map/radius/<origin>/<dest>/<radius>')
-def get_filtered_ticks(origin, dest, radius):
-    result = []
-    for d in dest:
-        url = f'https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={d}&key=AIzaSyCi4Z6r3IAxS0ywrRniNwvzUFreM7poFyk'
-        res = requests.get(url)
-        if res.status_code == 200 and res.json()['rows'][0]['elements'][0]['status'] != 'NOT_FOUND':
-            dist = res.json()['rows'][0]['elements'][0]['distance']['text']
-            dur = res.json()['rows'][0]['elements'][0]['duration']['text']
-            if dist <= radius:
-                result.append({'dist': dist, 'dur': dur})
-    return result
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='flask_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
-@api.route('/profile', methods=['GET'])
-def my_profile():
-    return 'Hello World'
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return books
