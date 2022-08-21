3b5a4bb62e2ae19a56b707cc6f1b9be44784fda6
Daniel Fang <daniel@Daniels-MacBook-Air.local>
2022-08-20 23:56:25 -0400

    login in progress

--- frontend/src/views/ContractorLoginPage.js
+++ frontend/src/views/ContractorLoginPage.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 import Map from "../assets/map.jpg";
+import axios from "axios";
+
 function LandingPage() {
   const navigate = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const createAccountRouteChange = () => {
     const path = "/create-account";
     navigate(path);
   };
+
+  // useEffect(() => {
+  //   axios({
+  //     method: "post",
+  //     url: "http://localhost:5000/login",
+  //     data: {
+  //       email: email,
+  //       password: password,
+  //     },
+  //   }).then((res) => {
+  //     console.log(res);
+  //   });
+  // }), [email, password];
 
   return (
     <div>
@@ -22,6 +40,7 @@ function LandingPage() {
               placeholder="example@email.com"
               id="#email"
               type="email"
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
             <span className="label">Password</span>
             <input
@@ -29,9 +48,10 @@ function LandingPage() {
               placeholder="e.g. ilovedavid"
               id="#password"
               type="password"
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
             <hr />
-            <button className="login-button">Login</button>
+            <button className="login-button" onClick={() => {navigate("/map", { replace: true }); }}>Login</button>
             <p className="createAccountText">
               Don't have an account?{" "}
               <span
