db3a23002ca19b58ef7ad0641ffa500cf444e459
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-08-21 01:51:01 -0400

    Complete MapInterface

--- frontend/src/views/CreatePost.js
+++ frontend/src/views/CreatePost.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import AddImage from "../components/AddImage";
 
 import "./CreatePost.css";
 
 function CreatePost() {
+  const navigate = useNavigate();
+  const mapRouteChange = () => {
+    const path = "/map";
+    navigate(path);
+  };
+
   return (
     <div className="main-create">
+      <p className="warepair">Warepair</p>
       <div className="center-div">
         <form className="post-form">
           <span className="images-label">Upload Images</span>
@@ -33,7 +41,12 @@ function CreatePost() {
           <input className="price"></input>
           <span className="description-label">Description</span>
           <textarea className="description"></textarea>
-          <button className="next-button">Next</button>
+          <div className="button-container">
+            <button classname="cancel-button" onClick={mapRouteChange}>
+              Cancel
+            </button>
+            <button className="next-button">Next</button>
+          </div>
         </form>
       </div>
     </div>
--- frontend/src/views/MapInterface.js
+++ frontend/src/views/MapInterface.js
@@ -1,12 +1,21 @@
-import React, { Component, useState, useRef, useEffect } from 'react';
-import { GoogleMap, InfoWindow, LoadScript, Marker, useJsApiLoader, DistanceMatrixService } from '@react-google-maps/api';
+import React, { Component, useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  GoogleMap,
+  InfoWindow,
+  LoadScript,
+  Marker,
+  useJsApiLoader,
+  DistanceMatrixService,
+} from "@react-google-maps/api";
 import axios from "axios";
 
-const MapInterface = () => {
+import "./MapInterface.css";
 
+const MapInterface = () => {
   const baseURL = "http://127.0.0.1:5000";
-  
-  const [helpLocation, setHelpLocation] = useState({})
+
+  const [helpLocation, setHelpLocation] = useState({});
 
   // const [requests, setRequests] = useState();
 
@@ -17,8 +26,8 @@ const MapInterface = () => {
   // }, []);
 
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyCi4Z6r3IAxS0ywrRniNwvzUFreM7poFyk"
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCi4Z6r3IAxS0ywrRniNwvzUFreM7poFyk",
   });
 
   const defaultMapOptions = {
@@ -27,13 +36,13 @@ const MapInterface = () => {
   };
 
   const containerStyle = {
-    width: '100vw',
-    height: '100vh'
+    width: "100vw",
+    height: "100vh",
   };
-  
+
   const center = {
-  lat: -3.745,
-  lng: -38.523
+    lat: -3.745,
+    lng: -38.523,
   };
 
   const helpRequests = [
@@ -42,66 +51,97 @@ const MapInterface = () => {
       description: "Help me unclog toilet!",
       location: {
         lat: -3.745,
-        lng: -38.523
+        lng: -38.523,
       },
       contact: {
         phone: "647-647-6477",
-        email: "myemail@gmail.com"
-      }
+        email: "myemail@gmail.com",
+      },
     },
     {
       title: "Clogged Urinal",
       description: "Help me unclog urinal!",
       location: {
         lat: 43.66336,
-        lng: -79.3870336
+        lng: -79.3870336,
       },
       contact: {
         phone: "647-647-6477",
-        email: "myemail@gmail.com"
-      }
-    }
+        email: "myemail@gmail.com",
+      },
+    },
   ];
 
-  return ( isLoaded ? (
+  const navigate = useNavigate();
+  const landingRouteChange = () => {
+    const path = "/";
+    navigate(path);
+  };
+  const createPostRouteChange = () => {
+    const path = "/create-post";
+    navigate(path);
+  };
+
+  return isLoaded ? (
+    <div className="main-container">
+      <p className="warepair">Warepair</p>
+      <button className="sign-out-button" onClick={landingRouteChange}>
+        Sign Out
+      </button>
+      <div className="sidebar">
+        <div className="post-container">
+          <button
+            className="create-post-button"
+            onClick={createPostRouteChange}
+          >
+            Create Post
+          </button>
+        </div>
+      </div>
       <GoogleMap
-          center={center}
-          mapContainerStyle={containerStyle}
-          zoom={15}
-          options={defaultMapOptions}
-          onClick={() => setHelpLocation({})}
+        center={center}
+        mapContainerStyle={containerStyle}
+        zoom={15}
+        options={defaultMapOptions}
+        onClick={() => setHelpLocation({})}
       >
-          {
-            helpRequests.map(item => {
-              return (
-                <Marker 
-                position={item.location}
-                onClick={() => {
-                  setHelpLocation(item);
-                }}
-                />
-              )
-            })
-          }
-          {
-            helpLocation.location && (
-              <InfoWindow
-              position={helpLocation.location}
-              clickable={true}
-              onCloseClick={() => setHelpLocation({})}>
-                <div>
-                  <img src={'https://fox5sandiego.com/wp-content/uploads/sites/15/2022/05/okmulgeeimage2crop.jpg?w=768'} width='300px' style={{marginTop: 10}}/>
-                  <h2>{helpLocation.title}</h2>
-                  <p>{helpLocation.contact.phone}</p>
-                  <p>{helpLocation.contact.email}</p>
-                  <p>{helpLocation.description}</p>
-                </div>
-              </InfoWindow>
-            )
-          }
+        {helpRequests.map((item) => {
+          return (
+            <Marker
+              position={item.location}
+              onClick={() => {
+                setHelpLocation(item);
+              }}
+            />
+          );
+        })}
+        {helpLocation.location && (
+          <InfoWindow
+            position={helpLocation.location}
+            clickable={true}
+            onCloseClick={() => setHelpLocation({})}
+          >
+            <div>
+              <img
+                src={
+                  "https://fox5sandiego.com/wp-content/uploads/sites/15/2022/05/okmulgeeimage2crop.jpg?w=768"
+                }
+                width="300px"
+                alt="Clogged Toilet"
+                style={{ marginTop: 10 }}
+              />
+              <h2>{helpLocation.title}</h2>
+              <p>{helpLocation.contact.phone}</p>
+              <p>{helpLocation.contact.email}</p>
+              <p>{helpLocation.description}</p>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
-  ) : <></>
-  )
-}
+    </div>
+  ) : (
+    <h1>The Map Cannot Load</h1>
+  );
+};
 
 export default MapInterface;
